8886c85a54b5c6d5bb59830af0fee05b3b203d96
rabiulim <rabiulim2022@gmail.com>
2022-05-10 20:42:42 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>Rabiul Islam||2022</p>
+            <p>All the Rights Only for Ower</p>
+        </div>
+    );
+};
+
+export default Footer;
