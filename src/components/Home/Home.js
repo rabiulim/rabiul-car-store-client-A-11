1ab1a7d8549635999e209f1d5445fb426623ca56
rabiulim <rabiulim2022@gmail.com>
2022-05-10 20:26:50 +0600

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            <div>
+                <div className="banner">
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://media.istockphoto.com/photos/car-is-standing-on-the-breakdown-lane-asphalt-and-tyre-italy-picture-id1319939893?b=1&k=20&m=1319939893&s=170667a&w=0&h=KkJ0ADibE2zQIBqyXtwnV-pTEukfGPkHQz3y483n3fA="
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
         </div>
     );
 };
