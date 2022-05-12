1758cd9a73ab54bc89ebe117b3e0589794cc1e71
rabiulim <rabiulim2022@gmail.com>
2022-05-12 21:52:19 +0600

    add item form added

--- src/components/AddItems/AddItems.js
+++ src/components/AddItems/AddItems.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const AddItems = () => {
     return (
-        <div>
-            <h1>Add item page</h1>
+        <div className='container text-center my-5'>
+            <form className='w-25 mx-auto' >
+                <h1 className='text-dark fw-b '>Add Your Item</h1>
+                <input type="text" name='name' placeholder='Name' /> <br />
+                <input type="email" name='email' placeholder='Your Email' required /> <br />
+                <input type="text" name='password' placeholder='Description' required /> <br />
+                <input type="text" name='password' placeholder='Quantity' required /> <br />
+                <input type="text" name='password' placeholder='Price' required /> <br />
+                <button className='signup-submit' type="submit">Add Item</button>
+            </form>
         </div>
     );
 };
--- src/components/ManageItem/ManageItem.js
+++ src/components/ManageItem/ManageItem.js
@@ -6,7 +6,7 @@ const ManageItem = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('fakedata.json')
+        fetch('http://localhost:5000/car')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
